06a71a6078dcc816eb6802a1feed9c776828f638
EricNiyo73 <ericniyokwizerwa1@gmail.com>
2024-02-15 11:22:27 +0200

    handle image uploading

---
+++ UI/script/AddBlo2.js
@@ -0,0 +1,82 @@
+let details = [];
+
+getData();
+
+function getData() {
+  let data = localStorage.getItem("blogs");
+  if (data) {
+    details = JSON.parse(data);
+  } else {
+    setData();
+  }
+}
+
+function setData() {
+  localStorage.setItem("blogs", JSON.stringify(details));
+}
+
+let blogImage = document.getElementById("blogImage");
+let image = "";
+
+blogImage.addEventListener("change", (e) => {
+  const img = e.target.files[0];
+
+  const reader = new FileReader();
+  reader.readAsDataURL(img);
+
+  reader.addEventListener("load", () => {
+    image = reader.result;
+  });
+});
+
+function save() {
+  let blogTitle = document.getElementById("blogTitle");
+  let blogContent = document.getElementById("blogDescription");
+  //   if (!image) {
+  //     alert("Please select an image");
+  //     return;
+  //   }
+  resetErrorMessages();
+
+  var isValid = true;
+  let data = {
+    blogTitle: blogTitle.value.trim(),
+    blogContent: blogContent.value.trim(),
+    image: image,
+  };
+  if (!data.blogTitle) {
+    displayErrorMessage("titleError", "Please enter a blog title");
+    isValid = false;
+  }
+  if (!data.blogContent) {
+    displayErrorMessage("descriptionError", "Please enter a blog description");
+    isValid = false;
+  }
+  if (!data.blogTitle) {
+    displayErrorMessage("imageError", "Please select an image");
+    isValid = false;
+  }
+  if (isValid) {
+    details.push(data);
+    setData();
+    blogTitle.value = "";
+    blogContent.value = "";
+    image = "";
+  }
+}
+
+function resetErrorMessages() {
+  document.getElementById("titleError").textContent = "";
+  document.getElementById("descriptionError").textContent = "";
+  document.getElementById("imageError").textContent = "";
+}
+
+function displayErrorMessage(id, message) {
+  var errorMessageElement = document.getElementById(id);
+  errorMessageElement.textContent = message;
+  errorMessageElement.style.color = "red";
+}
+document.getElementById("addBlog").addEventListener("click", function (e) {
+  e.preventDefault();
+  save();
+});
