5981b65970bb1f5095d0bfebb1d0c083881b045c
EricNiyo73 <ericniyokwizerwa1@gmail.com>
2024-02-19 11:55:09 +0200

    rotecting Dashboard pages [Javascript]

--- UI/script/Authentication.js
+++ UI/script/Authentication.js
@@ -2,15 +2,28 @@ function checkAuthentication() {
   var isLoggedIn = localStorage.getItem("isLoggedIn");
   return isLoggedIn === "true";
 }
+function checkAdmin(email, password) {
+  var registeredUsers = JSON.parse(localStorage.getItem("userData"));
+  if (registeredUsers) {
+    var user = registeredUsers.find(
+      (user) => user.email === email && user.password === password
+    );
+
+    if (user && user.isAdmin) {
+      return true;
+    }
+  }
+  return false;
+}
 
 function redirectToLoginPage() {
   window.location.href = "../Login.html";
 }
 
-function checkAccessToAdminMessagePage() {
-  if (!checkAuthentication()) {
+function checkAccessToAdminPage() {
+  if (!checkAuthentication() && !checkAdmin()) {
     redirectToLoginPage();
   }
 }
 
-checkAccessToAdminMessagePage();
+checkAccessToAdminPage();
---
+++ UI/script/loginToLogout.js
@@ -0,0 +1,19 @@
+// ======================change LOGIN TO LOGOUT========================
+
+function ToChangeLoginStatus() {
+  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+  const loginLink = document.getElementById("loginLink");
+
+  if (isLoggedIn) {
+    loginLink.innerHTML = '<a href="#" id="logout">LOGOUT</a>';
+    document.getElementById("logout").addEventListener("click", function () {
+      localStorage.removeItem("isLoggedIn");
+      ToChangeLoginStatus();
+    });
+  } else {
+    loginLink.innerHTML = '<a href="./Pages/Login.html">LOGIN</a>';
+    loginLink.style.color = "red";
+  }
+}
+
+ToChangeLoginStatus();
